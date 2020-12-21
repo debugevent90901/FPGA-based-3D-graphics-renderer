056a2807605b6c45b2b7075520b2fcbc939f44cb
TaKeTube <jamie.zimu.guan@gmail.com>
2020-12-21 13:44:28 +0800

    Can rotate a cube from 0 to pi/2 git add .! (glue projection & draw together and debugged for a long time. The most significant one is that we add 3 states waiting for the projection calculation or that the time would not converge.)

--- MidPoint/control_unit.sv
+++ MidPoint/control_unit.sv
@@ -4,10 +4,13 @@ module control_unit(input Clk,
                     input frame_clk,
                     input draw_done,
                     input clear_done,
+                    input load_done,
                     input [9:0] draw_DrawX, draw_DrawY,
                     input [9:0] clear_DrawX, clear_DrawY,
+                    output logic load_obj,
                     output logic draw_start,
                     output logic clear_start,
+                    output logic proj_start,
                     output logic draw_data,
                     output logic [9:0] DrawX, DrawY,
                     output logic frame_clk_rising_edge,
@@ -18,7 +21,7 @@ module control_unit(input Clk,
     // Clear: first clear the frame
     // Draw: then draw triangles
     // Done: wait until next frame is begin
-    enum logic [1:0] {Clear, Draw, Done} curr_state, next_state;
+    enum logic [1:0] {Init, Clear, Draw, Done} curr_state, next_state;
 
     logic frame_clk_delayed;
     always_ff @ (posedge Clk) begin
@@ -29,7 +32,7 @@ module control_unit(input Clk,
     always_ff @(posedge Clk)
     begin
         if (Reset)
-            curr_state <= Done;
+            curr_state <= Init;
         else
             curr_state <= next_state;
     end
@@ -39,12 +42,19 @@ module control_unit(input Clk,
         next_state = curr_state;
         draw_start = 1'b0;
         clear_start = 1'b0;
+        proj_start = 1'b0;
         draw_data = 1'b0;
         frame_done = 1'b0;
         DrawX = 10'b0;
         DrawY = 10'b0;
+        load_obj = 1'b0;
         
         unique case (curr_state)
+        Init:
+        begin
+            if(load_done)
+                next_state = Done;
+        end
         Clear:
         begin
             if(clear_done)
@@ -64,9 +74,14 @@ module control_unit(input Clk,
         endcase
 
         case (curr_state)
+        Init:
+        begin
+            load_obj = 1'b1;
+        end
         Clear:
         begin
             clear_start = 1'b1;
+            proj_start = 1'b1;
             DrawX = clear_DrawX;
             DrawY = clear_DrawY;
         end
